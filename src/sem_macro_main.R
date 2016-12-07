346bd7388b41cf78ae4348820f52eca004681add
Arnob L. Alam <arnoblalam@gmail.com>
2016-12-07 03:24:57 -0500

    Various

--- src/sem_macro_main.R
+++ src/sem_macro_main.R
@@ -33,17 +33,55 @@ eur_libor <- read.zoo(as.data.frame(remove_last_row(read_excel("data/EUR LIBOR.x
                         col_types = c("date", "numeric"),
                         skip = 4))))
 
-exchange_rate_data <- merge(fwd_points, spot_rates, usd_libor, eur_libor)
+eur_libor <- eur_libor/100
 
+usd_libor <- usd_libor/100
 
-exchange_rate_data$forward_rates <- exchange_rate_data$spot_rate + exchange_rate_data$fwd_points/10000
+forward_rates <- spot_rates + fwd_points/10000
 
-exchange_rate_data$swap_implied_forward_rates <- exchange_rate_data$spot_rate * ((1+exchange_rate_data$usd_libor/100)^.25)/((1+exchange_rate_data$eur_libor/100)^.25)
+swap_implied_rates <- ((forward_rates/spot_rates) * (1 + eur_libor)^0.25)^4 - 1
 
-plot(exchange_rate_data[,c("swap_implied_forward_rates", "forward_rates", 
-                           "spot_rates")], plot.type = "single", 
-     col = c("red", "blue", "green"))
+plot(merge(swap_implied_rates*100, usd_libor*100), 
+     plot.type = "single",
+     col = c("red", "blue"),
+     main = "3 month FX swap implied rates and
+     3 month USD Libor rates",
+     sub = "Bloomberg News, author's own calculations",
+     ylab = "Percentage")
+grid()
+legend("topright", 
+       legend = c("FX swap implied rate", "USD Libor 3M"), 
+       col = c("red", "blue"),
+       lty = c(1, 1))
 
-plot((exchange_rate_data$forward_rates - exchange_rate_data$swap_implied_forward_rates)*10000,
-     ylab = "CIP deviation (Basis Points)",
-     main = "Deviations from covered interest rate parity conditions")
+plot(window(merge(swap_implied_rates*100, usd_libor*100), 
+            start = "2006-09-01", 
+            end = "2008-09-01"), 
+     plot.type = "single",
+     col = c("red", "blue"),
+     main = "3 month FX swap implied rates and
+     3 month USD Libor rates (Sep '06 - Sep '08)",
+     sub = "Bloomberg News, author's own calculations",
+     ylab = "Percentage")
+grid()
+legend("topright", 
+       legend = c("FX swap implied rate", "USD Libor 3M"), 
+       col = c("red", "blue"),
+       lty = c(1, 1))
+bofa_cds <- read_excel("~/sem_macro/data/BOFA CDS.xlsx", 
+                       col_types = c("date", "numeric"), 
+                       skip = 5)
+
+# plot(window(swap_implied_rates*100, end="2008-01-01"))
+# plot(window(forward_rates/spot_rates, end="2008-01-01"))
+# plot(window(fwd_points, end="2008-01-01")) 
+# 
+# plot(window(usd_libor*100, start="2007-07-01", end="2008-02-01"), ylim=c(2.8, 5.8))
+# plot(window(eur_libor*100, start="2007-07-01", end="2008-02-01"), ylim=c(3, 5))
+# 
+# plot(window(swap_implied_rates, start="2007-01-01", end="2008-01-01"),
+#      plot.type = "single",
+#      col = c("red", "blue"))
+# 
+# 
+# 
