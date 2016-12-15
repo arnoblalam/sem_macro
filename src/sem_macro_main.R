fee43b045ca8c4d93cb5079b37cbbb2d5edda0b4
Arnob Alam <arnoblalam@gmail.com>
2016-12-15 16:06:17 -0500

    Add North American IG CDS data

--- src/sem_macro_main.R
+++ src/sem_macro_main.R
@@ -55,15 +55,57 @@ ted_spread <- read.zoo(read.xlsx2(
 ))
 
 # CDS rates
+# Major US financials (Bank of America, Citibank and JP Morgan)
 bofa_cds <- read_data("data/BOFA CDS.xlsx")
 
 cinc_cds <- read_data("data/CINC CDS.xlsx")
 
 jpm_cds <- read_data("data/JMPCC CDS.xlsx")
 
+# Major European banks
 rabobank_cds <- read_data("data/Rabobank CDS.xlsx")
 
 deutsche_cds <- read_data("data/Deutsche Bank CDS.xlsx")
+
+#Investment grade American banks
+agmc_cds <- read_data("data/AGMC CDS.xlsx")
+
+aig_cds <- read_data("data/AIG CDS.xlsx")
+
+allstate_cds <- read_data("data/Allstate CDS.xlsx")
+
+amex_cds <- read_data("data/AMEX CDS.xlsx")
+
+berkshire_cds <- read_data("data/Berkshire CDS.xlsx")
+
+cap_one_cds <- read_data("data/Capital One CDS.xlsx")
+
+chubb_cds <- read_data("data/Chubb CDS.xlsx")
+
+erp_cds <- read_data("data/ERP Financial CDS.xlsx")
+
+hartford_cds <- read_data("data/Hartford CDS.xlsx")
+
+host_cds <- read_data("data/Host Hotels CDS.xlsx")
+
+international_lease_cds <-
+  read_data("data/International Lease Financing Corp CDS.xlsx")
+
+lincoln_cds <- read_data("data/Lincoln CDS.xlsx")
+
+lowes_cds <- read_data("data/Lowes CDS.xlsx")
+
+marsh_cds <- read_data("data/Marsh CDS.xlsx")
+
+metlife_cds <- read_data("data/METLIFE CDS.xlsx")
+
+nr_cds <- read_data("data/National Rural CDS.xlsx")
+
+prudential_cds <- read_data("data/Prudential CDS.xlsx")
+
+simon_cds <- read_data("data/Simon CDS.xlsx")
+
+weyhauser_cds <- read_data("data/Weyerhaeuser CDS.xlsx")
 
 # Data cleaning
 
@@ -80,7 +122,7 @@ usd_libor <- usd_libor / 100
 
 eur_ois <- eur_ois / 100
 
-usd_ois <- usd_ois/100
+usd_ois <- usd_ois / 100
 
 # usd_ois <- usd_ois/100
 
