57f3ac97761f6c277978799677840da99f8f39dc
Arnob Alam <arnoblalam@gmail.com>
2016-12-23 22:58:31 -0500

    Playing around with analysis

--- src/analysis.R
+++ src/analysis.R
@@ -10,10 +10,20 @@ source("src/load_data.R")
 library(strucchange)
 library(rugarch)
 
-# model_1 <- lm(cipv ~ ., data = main_data)
-# e <- resid(model_1)
+main_data <- merge(cipv = swap_implied_rates - usd_libor,
+                   na_ig_cdx,
+                   eur_ig_cdx = european_fin_cds,
+                   broad_usd = (eurodollar - usd_ois),
+                   broad_eur = (euribor - eur_ois),
+                   ecb_operations)
+main_data$ecb_operations <- na.fill(main_data$ecb_operations, list(0, 0, 0))
 
 main_data_c <- main_data[complete.cases(main_data),]
+pre_2010 <- window(main_data_c, end = "2009-12-31")
+
+
+model_1 <- lm(cipv ~ ., data = main_data_c)
+e <- resid(model_1)
 
 spec <- ugarchspec(
   variance.model = list(
@@ -22,7 +32,7 @@ spec <- ugarchspec(
   mean.model = list(
     armaOrder = c(0, 0),
     include.mean = TRUE,
-    external.regressors = as.matrix(main_data_c[,-1])
+    external.regressors = as.matrix(main_data_c[,-1:-3])
     ))
 
 fit <- ugarchfit(spec, main_data_c[,1])
