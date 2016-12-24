bc37a92aa5b0eaeafb0c9436e53dadd629b299d0
Arnob Alam <arnoblalam@gmail.com>
2016-12-23 20:50:54 -0500

    eGarch analysis

--- src/analysis.R
+++ src/analysis.R
@@ -5,9 +5,24 @@
 # This is the main analysis file for the analysis of counterparty risk premia
 # during financial crisis in the FX market
 
+source("src/load_data.R")
 
 library(strucchange)
 library(rugarch)
 
-model_1 <- lm(cipv ~ ., data = main_data)
-plot(gefp(cipv ~ ., data = window(main_data)))
+# model_1 <- lm(cipv ~ ., data = main_data)
+# e <- resid(model_1)
+
+main_data_c <- main_data[complete.cases(main_data),]
+
+spec <- ugarchspec(
+  variance.model = list(
+    model = 'eGARCH',
+    garchOrder = c(1, 1)),
+  mean.model = list(
+    armaOrder = c(0, 0),
+    include.mean = TRUE,
+    external.regressors = as.matrix(main_data_c[,-1])
+    ))
+
+fit <- ugarchfit(spec, main_data_c[,1])
