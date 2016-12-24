e0f441498e68f6a4a87dfa82a71d9edc8d873ce6
Arnob Alam <arnoblalam@gmail.com>
2016-12-23 21:21:33 -0500

    Set time zone to GMT

--- src/helper.R
+++ src/helper.R
@@ -9,5 +9,6 @@ remove_last_row <- function(x) {
 read_data <- function(path, col_types = c("date", "numeric"), skip = 5) {
   read.zoo(as.data.frame(remove_last_row(read_excel(path, 
                                                     col_types = col_types,
-                                                    skip = skip))))
+                                                    skip = skip))),
+           tz = "GMT")
 }
